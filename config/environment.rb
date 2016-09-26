52208f6322a910378af2186b8a0f89da9a782c1b
AdrianManteza <admanteza@mail.com>
2016-09-26 21:45:10 +0000

    ready for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ require 'sinatra/contrib/all' # Requires cookies, among other things
 require 'google_custom_search_api'
 require 'net/http'
 require 'json'
+require 'pry' if development?
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
